d949ccf281ad606e03da75bd2be16fcb35ba4ef9
sandy <sun@sandy>
2018-11-06 12:20:59 +0530

    Initial commit

---
+++ src/main/java/com/sandy/crudsand/CrudsandApplication.java
@@ -0,0 +1,12 @@
+package com.sandy.crudsand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudsandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudsandApplication.class, args);
+    }
+}
